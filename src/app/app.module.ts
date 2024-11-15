7ce5c94e39e57ad584c025b4e74d8b8d7f582b06
Matheus Maestri <matheusmaestri28@gmail.com>
2024-11-15 14:47:35 -0300

    criando componentes para estudos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuTituloComponent } from './components/menu-titulo/menu-titulo.component';
+import { CardGrandeComponent } from './components/card-grande/card-grande.component';
+import { CardPequenoComponent } from './components/card-pequeno/card-pequeno.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuTituloComponent,
+    CardGrandeComponent,
+    CardPequenoComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
